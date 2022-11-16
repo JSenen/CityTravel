7a5e8d989f1889f4387e1707b9ec004ee2082779
Juan Senen <95066970+JSenen@users.noreply.github.com>
2022-11-16 18:56:28 +0100

    Inicio MOdelMapper

---
+++ src/main/java/com/juansenen/citytravel/LineConfig.java
@@ -0,0 +1,15 @@
+package com.juansenen.citytravel;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.ModelMap;
+
+@Configuration
+public class LineConfig {
+
+    //TODO ModelMapper
+   // @Bean
+   // public ModelMap modelMap(){
+   //     return new modelmap();
+   //}
+}
